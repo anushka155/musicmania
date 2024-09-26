f1a75a890730ae8c632c370c50273610d50e245a
Anuska Gurung <annggrg1@gmail.com>
2024-09-26 23:23:49 +0545

    line

--- configs/appConst.js
+++ configs/appConst.js
@@ -1,4 +1,6 @@
 export const MONGODB_URI =
-  "mongodb+srv://dubeydon:xdksLmXEcsX_Zm9@cluster0.zrmmdlj.mongodb.net/musicmania?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://annggrg:5nzk-b7NG_sLfcm@cluster0.zsvdrja.mongodb.net/Musicmania?retryWrites=true&w=majority&appName=Cluster0";
 
+
+  
 export const APP_KEY = "my_app_key";
